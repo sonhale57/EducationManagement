c106218e89d7bfa1409d2b8ea075a9dbc347f897
sonhale57 <sonhale57@gmail.com>
2024-09-04 08:13:02 +0700

    .

---
+++ Controllers/ConfigurationsController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using SuperbrainManagement.Models;
+
+namespace SuperbrainManagement.Controllers
+{
+    public class ConfigurationsController : Controller
+    {
+        private ModelDbContext db = new ModelDbContext();
+
+        // GET: Configurations
+        public ActionResult Index()
+        {
+            var configurations = db.Configurations.Include(c => c.User);
+            return View(configurations.ToList());
+        }
+
+        // GET: Configurations/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Configuration configuration = db.Configurations.Find(id);
+            if (configuration == null)
+            {
+                return HttpNotFound();
+            }
+            return View(configuration);
+        }
+
+        // GET: Configurations/Create
+        public ActionResult Create()
+        {
+            ViewBag.IdUser = new SelectList(db.Users, "Id", "Name");
+            return View();
+        }
+
+        // POST: Configurations/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,VAT,UsageFee,AccountFee,TimePayment,DiscountGroup1,DiscountGroup2,DiscountGroup3,DiscountGroup4,DiscountGroup5,DiscountGroup6,DiscountGroup7,IdUser")] Configuration configuration)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Configurations.Add(configuration);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.IdUser = new SelectList(db.Users, "Id", "Name", configuration.IdUser);
+            return View(configuration);
+        }
+
+        // GET: Configurations/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Configuration configuration = db.Configurations.Find(id);
+            if (configuration == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.IdUser = new SelectList(db.Users, "Id", "Name", configuration.IdUser);
+            return View(configuration);
+        }
+
+        // POST: Configurations/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,VAT,UsageFee,AccountFee,TimePayment,DiscountGroup1,DiscountGroup2,DiscountGroup3,DiscountGroup4,DiscountGroup5,DiscountGroup6,DiscountGroup7,IdUser")] Configuration configuration)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(configuration).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.IdUser = new SelectList(db.Users, "Id", "Name", configuration.IdUser);
+            return View(configuration);
+        }
+
+        // GET: Configurations/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Configuration configuration = db.Configurations.Find(id);
+            if (configuration == null)
+            {
+                return HttpNotFound();
+            }
+            return View(configuration);
+        }
+
+        // POST: Configurations/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Configuration configuration = db.Configurations.Find(id);
+            db.Configurations.Remove(configuration);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
