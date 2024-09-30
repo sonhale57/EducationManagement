ea1bcae9f8863310e221c7c87d0d811a22aa2c9c
sonhale57 <sonhale57@gmail.com>
2024-09-30 10:02:58 +0700

    commit change

--- Controllers/EmployeesController.cs
+++ Controllers/EmployeesController.cs
@@ -13,6 +13,7 @@ using System.Threading.Tasks;
 using SuperbrainManagement.Helpers;
 using static Org.BouncyCastle.Math.EC.ECCurve;
 using System.Xml.Linq;
+using System.IO;
 
 namespace SuperbrainManagement.Controllers
 {
@@ -30,7 +31,7 @@ namespace SuperbrainManagement.Controllers
             }
             else
             {
-                var branches = db.Branches.Where(x=>x.Enable==true).ToList();
+                var branches = db.Branches.Where(x => x.Enable == true).ToList();
                 int idbranch = int.Parse(CheckUsers.idBranch());
                 if (!CheckUsers.CheckHQ())
                 {
@@ -54,7 +55,7 @@ namespace SuperbrainManagement.Controllers
                 }
                 ViewBag.CurrentFilter = searchString;
 
-                var employees = db.Employees.Where(x=>x.Enable==true).ToList();
+                var employees = db.Employees.Where(x => x.Enable == true).ToList();
                 if (!string.IsNullOrEmpty(filterEnum))
                 {
                     employees = employees.Where(x => x.IsOfficial == Boolean.Parse(filterEnum)).ToList();
@@ -109,6 +110,62 @@ namespace SuperbrainManagement.Controllers
                 return View(pagedData);
             }
         }
+        public ActionResult Loadlist(bool IsOfficial, int? IdBranch, string searchString)
+        {
+            // Lấy ID của branch người dùng hiện tại
+            int idbranch = Convert.ToInt32(CheckUsers.idBranch());
+
+            // Lọc danh sách nhân viên
+            var emp = db.Employees.Where(x => x.Enable == true && x.IsOfficial == IsOfficial);
+
+            // Nếu IdBranch không có giá trị, lấy IdBranch của user đăng nhập
+            if (IdBranch == null)
+            {
+                IdBranch = idbranch;
+            }
+
+            // Lọc theo IdBranch
+            emp = emp.Where(x => x.IdBranch == IdBranch);
+
+            // Nếu searchString không rỗng, thực hiện tìm kiếm
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                emp = emp.Where(x => x.Name.Contains(searchString));
+            }
+
+            // Biến chứa chuỗi HTML để render danh sách
+            string str = "";
+            int stt = 0;
+
+            // Duyệt qua từng nhân viên và tạo hàng cho bảng
+            foreach (var item in emp)
+            {
+                stt++;
+                str += "<tr>"
+                    + "<td class='text-center align-content-center'>" + stt + "</td>"
+                    + "<td class='text-start align-content-center'>" + item.Name + "</td>"
+                    + "<td class='text-center align-content-center'>" + item.Phone + "</td>"
+                    + "<td class='text-center align-content-center'>" + item.Email + "</td>"
+                    + "<td class='text-center align-content-center'>" + (item.IdPosition == null ? "-" : item.Position.Name) + "</td>"
+                    + "<td class='text-center align-content-center'>" + (item.StartWork == null ? "-" : item.StartWork.Value.ToString("dd/MM/yyyy")) + "</td>"
+                    + "<td class='text-center align-content-center'>" + (item.CertificateNumber == null ? "-" : item.CertificateNumber.ToString()) + "</td>"
+                    + "<td class='text-end align-content-center'>"
+                    + "<a href=\"/employees/edit/" + item.Id + "\" class=\"me-1\"><i class=\"ti ti-edit text-primary\"></i></a>"
+                    + "<a href=\"javascript:Delete(" + item.Id + ")\" class=\"me-1\"><i class=\"ti ti-trash text-danger\"></i></a>"
+                    + "<a class=\"text-warning\" id=\"dropdownMenuButton\" data-bs-toggle=\"dropdown\" aria-expanded=\"false\">"
+                    + "<i class=\"ti ti-dots-vertical\"></i>"
+                    + "</a>"
+                    + "<ul class=\"dropdown-menu\" aria-labelledby=\"dropdownMenuButton\">"
+                    + "<li><a class=\"dropdown-item\" href=\"javascript:UserbyId(" + item.Id + ")\"><i class=\"ti ti-lock\"></i> Tài khoản đăng nhập</a></li>"
+                    + "</ul>"
+                    + "</td>"
+                    + "</tr>";
+            }
+
+            // Trả về chuỗi HTML dạng JSON
+            return Json(new { str }, JsonRequestBehavior.AllowGet);
+        }
+
         public static string GetLastName(string fullName)
         {
             if (string.IsNullOrEmpty(fullName))
@@ -128,9 +185,8 @@ namespace SuperbrainManagement.Controllers
 
             return phoneNumber.Substring(phoneNumber.Length - 3);
         }
-        public ActionResult Submit_AddPosition(string Code, string Name,string Action)
+        public ActionResult Submit_AddPosition(string Code, string Name, string Action)
         {
-
             string status = "ok";
             string message = "";
             var pc = db.Positions.SingleOrDefault(x => x.Code == Code && x.Enable == true);
@@ -162,6 +218,118 @@ namespace SuperbrainManagement.Controllers
             };
             return Json(item, JsonRequestBehavior.AllowGet);
         }
+        public ActionResult Submit_create(string Name, string Phone, string Email, string Sex, string Address, string Description, DateTime DateOfBirth, DateTime StartWork, string Gratuate, int? IdBranch, int IdPosition, HttpPostedFileBase imageFile)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    if (IdBranch == null)
+                    {
+                        IdBranch = Convert.ToInt32(CheckUsers.idBranch());
+                    }
+                    var e = new Employee()
+                    {
+                        Name = Name,
+                        Phone = Phone,
+                        Email = Email,
+                        Sex = Sex,
+                        Address = Address,
+                        Description = Description,
+                        DateOfBirth = DateOfBirth,
+                        StartWork = StartWork,
+                        Gratuate = Gratuate,
+                        IdBranch = IdBranch,
+                        IdPosition = IdPosition,
+                        DateCreate = DateTime.Now
+                    };
+                    // Xử lý việc lưu ảnh đại diện (nếu có)
+                    if (imageFile != null && imageFile.ContentLength > 0)
+                    {
+                        string fileName = Path.GetFileName(imageFile.FileName);
+                        string filePath = Path.Combine(Server.MapPath("~/Uploads/Images"), fileName);
+                        imageFile.SaveAs(filePath);
+                        e.Image = "/Uploads/Images/" + fileName;
+                    }
+
+                    // Thực hiện thêm mới nhân viên vào cơ sở dữ liệu
+                    db.Employees.Add(e);
+                    db.SaveChanges();
+
+                    return Json(new { status = "ok", message = "Thành công: Đã thêm mới nhân sự thành công!" }, JsonRequestBehavior.AllowGet);
+                }
+                catch (Exception ex)
+                {
+                    return Json(new
+                    {
+                        status = "error",
+                        message = "Lỗi cập nhật: " + ex.Message
+                    });
+                }
+            }
+            return Json(new { status = "error", message = "Lỗi cập nhật: Dữ liệu không hợp lệ." });
+        }
+        public JsonResult CreateUser(int IdEmployee, string Name, string Password)
+        {
+            try
+            {
+                // Băm mật khẩu bằng MD5
+                MD5Hash md5 = new MD5Hash();
+                Password = md5.GetMD5Working(Password);
+                string pass = md5.mahoamd5(Password.Replace("&^%$", ""));
+
+                // Lấy thông tin nhân sự từ bảng Employees
+                var employee = db.Employees.Find(IdEmployee);
+                if (employee == null)
+                {
+                    return Json(new { success = false, message = "Nhân viên không tồn tại." });
+                }
+
+                var IdBranch = employee.IdBranch;
+                var Phone = employee.Phone;
+
+                // Khởi tạo ConvertText để xử lý tên
+                ConvertText convi = new ConvertText();
+                string baseUsername = "GV_" + convi.fixtex(GetLastName(Name)) + GetLastThreeDigits(Phone);
+
+                // Kiểm tra xem Username đã tồn tại hay chưa
+                string newUsername = baseUsername;
+                int count = 1;
+
+                while (db.Users.Any(u => u.Username == newUsername))
+                {
+                    // Tạo Username mới nếu bị trùng
+                    newUsername = baseUsername + count.ToString();
+                    count++;
+                }
+
+                // Tạo user mới
+                var newUser = new User()
+                {
+                    IdEmployee = IdEmployee,
+                    Name = Name,
+                    Username = newUsername, // Sử dụng Username mới tạo
+                    Password = pass,
+                    Enable = true,
+                    Active = true,
+                    DateCreate = DateTime.Now,
+                    IdBranch = IdBranch,
+                    Createby = Convert.ToInt32(CheckUsers.iduser()),
+                    Expire = DateTime.Now.AddMonths(6)
+                };
+
+                // Thêm vào database và lưu thay đổi
+                db.Users.Add(newUser);
+                db.SaveChanges();
+
+                return Json(new { success = true, message = "User đã được tạo thành công." });
+            }
+            catch (Exception ex)
+            {
+                return Json(new { success = false, message = "Lỗi: " + ex.Message });
+            }
+        }
+
         [HttpPost]
         public ActionResult Submit_adduser(int IdEmployee,string Username,string Name,string Password)
         {
